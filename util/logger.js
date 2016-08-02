ebccbee8ea74a54c15320c7a2af4153539d2e607
chalupaul <me@chalupaul.com>
2016-08-02 02:45:37 -0500

    Added capability to create logdir and base log location in config.
    Also changed default port to 3000

--- util/logger.js
+++ util/logger.js
@@ -1,11 +1,28 @@
+var fs = require('fs');
+var path = require('path');
 var winston = require('winston');
+var mkdirp = require('mkdirp');
+var conf = require('./config');
+
 winston.emitErrs = true;
+
+//Ensule log directory exists so we can write to files there
+var logfile = conf.get('server.logfile') || './logs/all-logs.log';
+fs.stat(logfile, function(err, stat) {
+	if (err) {
+		var dirname = require('path').dirname(logfile);
+		mkdirp(dirname, function(err) {
+			if (err) console.error("Could not create log directory.", err)
+		});
+	}
+})
+
 
 var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'info',
-            filename: './logs/all-logs.log',
+            filename: logfile,
             handleExceptions: true,
             json: true,
             maxsize: 5242880, //5MB
